a4bda45776bd709a0ccba142871f7fa69933ed82
Hector Vergara <hector.vergara416@gmail.com>
2021-01-09 00:41:33 +0800

    ci: add files needed to deploy in gh pages

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,8 @@ module.exports = {
               args[0].title = "Hector Vergara";
               return args;
           })
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-website/'
+    : '/'
 }
